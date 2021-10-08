63eda6b4af1716d54313b8f4700053d8fb3c0f01
AnneOlsen <annejko@hotmail.com>
2021-10-08 14:11:44 +0200

    swagger

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -13,11 +15,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     @Bean
-    public Docket swaggerUI(){
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo());
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("/designers.*"))
+                .build();
     }
 
     public ApiInfo apiInfo(){
-        return new ApiInfoBuilder().title("Designer API").build();
+        return new ApiInfoBuilder()
+                .title("Designer API Documentation 😎")
+                .build();
     }
 }
